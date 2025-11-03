a9520bb867b572245383a74320aed0d338231c5c
Sarah Jefferson <jeffersonsarah@myocu.oak.edu>
2025-11-03 13:07:43 -0600

    final commit

--- main.py
+++ main.py
@@ -18,29 +18,40 @@ def main():
     head = ll.insert_at_end(head, 25)
     head = ll.insert_at_end(head, 50)
     head = ll.insert_at_end(head, 75)
-    ### saving it to the head each time because it is returning the previous data to head each time
+    ll.display(head)
 
     # Element insertion at the beginning
-    head = ll.insert_at_beginning(100)
-    head = ll.insert_at_beginning(200)
+    print("Inserting elemtents at the beginning of the link list")
+    head = ll.insert_at_beginning(head, 100)
+    head = ll.insert_at_beginning(head, 200)
     ll.display(head)
-    ### add display at the end of most of functions
 
     # Get the length of the ll
-    ll.len(head)
-    ll.display(head)
+    print("Getting the length of the link list")
+    print("Legth of link list:", ll.get_length(head))
 
     # Search for an element in the ll
-
+    print("Searching for an element in the link list.")
+    print("It is in position:", ll.search(head, 50))
+    
     # Search for an element that does not exist in the ll
-        # Think about using conditional logic to handle this case(will be the same as above just adding if 
-        # else and using comparisions)
+    print("Searching for an element that does not exist in the link list.")
+    result = ll.search(head, 20)
+    
+    if result == -1:
+        print("That node is not in the link list.")
+    else:
+        print(f"That node is in position {result}")
+
 
     # Delete an element in the ll
-    head = ll.del(50)
+    print("Deleting an element from the link list.")
+    ll.delete_node(head, 100)
     ll.display(head)
 
     # Get final length of the ll (very similar to line 19)
+    print("Getting the final length of the link list")
+    print("Final length of link list:", ll.get_length(head))
 
 
 if __name__ == "__main__":
